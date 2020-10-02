1515816c5a86823a162f2a16fb406499e19e00c0
kazumoto <motch0214@gmail.com>
2020-10-02 15:47:47 +0900

    feat: Setup scss

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head"
 import React from "react"
 
-import styles from "../styles/Home.module.css"
+import styles from "../styles/Home.module.scss"
 
 const Index: React.FC = () => {
   return (
