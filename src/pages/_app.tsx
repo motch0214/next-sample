e51cdd1cd956e8ec2ad3de1fb40f5f31340e24f9
kazumoto <motch0214@gmail.com>
2020-10-20 17:38:16 +0900

    fix: Material UI styles with jss

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from "next/app"
-import React, { useEffect } from "react"
+import React from "react"
 
 import { ThemeProvider, StylesProvider } from "@material-ui/core/styles"
 
@@ -11,12 +11,6 @@ import theme from "../styles/theme"
 import "../styles/globals.css"
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
-  useEffect(() => {
-    // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector("#jss-server-side")
-    jssStyles?.parentElement?.removeChild(jssStyles)
-  }, [])
-
   return (
     <StylesProvider injectFirst>
       <ThemeProvider theme={theme}>
