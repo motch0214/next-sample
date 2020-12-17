d791946befa9983a4cf7103cb010d139150ada5a
kazumoto <motch0214@gmail.com>
2020-12-18 01:01:04 +0900

    feat: SEO

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -2,16 +2,28 @@ import React from "react"
 
 import { NextSeo } from "next-seo"
 
-const site = {
-  title: "Create Next App",
-}
-
-const Seo: React.FC<{ title?: string }> = ({ title }) => {
+const Seo: React.FC<{
+  title?: string
+  description?: string
+  image?: string
+}> = ({ title, description, image }) => {
   const meta = {
-    title: title ? `${title} | ${site.title}` : site.title,
+    title: title ? `${title} | Next Sample` : "Next Sample",
+    description: description || "Next project starter",
+    images: [{ url: image || `${process.env.NEXT_PUBLIC_SITE_URL}/meta.png` }],
+    type: "website",
+    site_name: "Next Sample",
   }
 
-  return <NextSeo title={meta.title} />
+  return (
+    <NextSeo
+      title={meta.title}
+      description={meta.description}
+      openGraph={meta}
+      noindex={process.env.NEXT_PUBLIC_BUILD_PROFILE !== "production"}
+      twitter={{ cardType: "summary_large_image" }}
+    />
+  )
 }
 
 export default Seo
